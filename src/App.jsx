2e8ef5e448ab1f8e5c03881de53faea12da25a9e
rjlddn.a <164434072+Nr017@users.noreply.github.com>
2024-06-16 10:24:48 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,19 @@
-// import React from "react";
-// import { Routes, Route } from "react-router-dom"
-// import SplashScreen from "./pages/SplashScreen";
-// import Login from "./pages/Login";
-// import Chat from "./pages/Chat";
-
-// export default function App() {
-
-//   return (
-//       <Routes>
-//         <Route path="/" element={<SplashScreen />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/chat" element={<Chat />} />
-//         <Route path="*" element={<h1>404 Page Not Found</h1>} />
-//       </Routes>
-//   )
-
-// }
-
 import React from "react";
 import { Routes, Route } from "react-router-dom"
 import SplashScreen from "./pages/SplashScreen";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
 
   return (
-    // <Router>
     <Routes>
       <Route path="/" element={<SplashScreen />} />
       <Route path="/login" element={<Login />} />
       <Route path="/chat" element={<Chat />} />
-      <Route path="*" element={<h1>404 Page Not Found</h1>} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
-    // </Router>
   )
 
 }
